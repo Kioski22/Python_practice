d7399b07594a96034b96e44a19d5abe5e3a3c187
kioski22 <floresrandy.m@gmail.com>
2025-07-20 18:47:54 +0800

    Create dict_forloop_practice.py
    practice of dictionary in python

---
+++ dict_forloop_practice.py
@@ -0,0 +1,10 @@
+x = {"randy" : "active" , "nicole" : "active","jericho" : "inactive","jemuel" : "active"} 
+#initiated a dictionary
+
+
+
+for name , status  in x.items(): # in this im looping over x storing the key in the name var and the value to the status var and 
+    if status == "inactive": # it checks  the  status  if it is inactive
+        print("this user : " + name + " is " + status) # once inactive is found in the status of the dictionary  it executes this printing code
+    elif name == "jemuel": # also it checks  if the name var is equal to "jemuel"
+        print("bading ka talaga ", name) # also if found that name var is equal to "jemuel" it executes this code 
