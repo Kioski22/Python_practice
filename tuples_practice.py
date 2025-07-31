bf32488de597f068df4ea6555290e31704477dd4
kioski22 <floresrandy.m@gmail.com>
2025-07-31 23:57:31 +0800

    this is learnign

---
+++ tuples_practice.py
@@ -0,0 +1,14 @@
+my_tuple = ( "this" , "is" , "a" , "tuple") 
+
+# my_tuple[4] = "sir!" # this adds the data to the index 4 of the tuple  
+# #but remember  that tuples are immutable 
+# print(my_tuple)  
+
+for i in my_tuple: #this iterates over the tuple and store it in the i variable 
+    my_tuple
+    print(i) # this prints the variable i  
+ 
+
+# access tuple  through its index 
+
+ # to print the  the type of the variable use the attribute type()s
