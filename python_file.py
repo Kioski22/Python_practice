d656cf724c61c8a80041653a51ecfb61d3a69f93
Randy Flores <floresrandy.m@gmail.com>
2025-08-17 07:20:48 -0700

    added try and except

--- python_file.py
+++ python_file.py
@@ -7,17 +7,20 @@
 print("-----------------python file creation--------------")
 
 def ask():
-    while True:
+     while True:
         file_name = input('name of the file:  ') 
         user_input = input('what do you want to input: ')
         File_creation(file_name, user_input)
         return file_name, user_input
+    
 
 
 def File_creation(file_name , user_input):
-    with open(f'{file_name}', 'w') as file:
-        file.writelines([f'{user_input}'])
-
+     try:
+         with open(f'{file_name}', 'a') as file:
+             file.writelines([f'{user_input}'])
+     except FileNotFoundError as e:
+         print("we couldn't found your file error: " , e)
 
 ask()
 
